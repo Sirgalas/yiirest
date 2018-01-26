203f3fb6b22b9abb6d8ce18d8b2fc5a1610cf797
sirgalas <hudos@rambler.ru>
2018-01-26 20:00:51 +0300

    redact misprints

--- widgets/views/comment/_form.php
+++ widgets/views/comment/_form.php
@@ -6,13 +6,13 @@ use yii\widgets\Pjax;
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 Pjax::begin(['id'=>'pjax_form']);
-$fotm = ActiveForm::begin([
+$form = ActiveForm::begin([
     'action'=>'comment/create'
 ]);
 ?>
-<?= $fotm->field($model,'id_aplication')->hiddenInput(['value'=>$id_apppication])->label(felse);?>
+<?= $form->field($model,'id_aplication')->hiddenInput(['value'=>$id_apppication])->label(false);?>
 <?= $form->field($model,'title'); ?>
-<?= $fotm->field($model,'comment')->textarea(); ?>
+<?= $form->field($model,'comment')->textarea(); ?>
 <div class="form-group">
     <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
 </div>
