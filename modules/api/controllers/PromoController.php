7af8a215f51a85131c08536ed1e78961946bc2d5
sirgalas <hudos@rambler.ru>
2018-02-02 09:48:12 +0300

    redact from heroku

--- modules/api/controllers/PromoController.php
+++ modules/api/controllers/PromoController.php
@@ -1,9 +1,11 @@
 <?php
 namespace app\modules\api\controllers;
 use app\entities\Promo;
+use app\entities\PromoCity;
 use Yii;
 use yii\rest\ActiveController;
 use yii\web\BadRequestHttpException;
+use yii\web\NotFoundHttpException;
 
 class PromoController extends ActiveController
 {
@@ -15,7 +17,8 @@ class PromoController extends ActiveController
 
     public function actionGetPromo(){
         $get= Yii::$app->request->get();
-        $promo=Promo::findOne(['name'=>$get['name']]);
+        if($promo=Promo::findOne(['name'=>$get['name']]))
+            throw new NotFoundHttpException('error request',404 );
         if(!empty($get['field'])){
             foreach ($get['field'] as $field){
                 $fields[$field]=$promo->$field;
@@ -23,17 +26,31 @@ class PromoController extends ActiveController
         }else{
             $fields=$promo;
         }
-        if(!$promo){
-            $this->setHeader(404);
-            return array('request'=>'Promo '.$get['name'].' not find');
+        try{
+            $this->setHeader(200);
+            return $fields;
+        }catch (NotFoundHttpException $ex){
+            $this->setHeader($ex->getCode());
+            return $ex->getMessage();
         }
-        $this->setHeader(200);
-        return $fields;
+
     }
 
     public function actionSetPromo(){
         $get=Yii::$app->request->get();
-
+        if(!isset($get['promo'])||!isset($get['city']))
+            throw new NotFoundHttpException('error request',400);
+        $sql="SELECT * FROM `promo` INNER JOIN promo_city ON promo_id=id WHERE name='".$get['promo']."' AND city_id=(SELECT id FROM city WHERE name='".$get['city']."')";
+        $promo=Promo::findBySql($sql)->one();
+        if(!$promo)
+            throw new NotFoundHttpException('result not found',404);
+        try{
+            $this->setHeader(200);
+            return array('sum'=>$promo->remuneration);
+        }catch (NotFoundHttpException $ex){
+            $this->setHeader($ex->getCode());
+            return $ex->getMessage();
+        }
     }
 
 }
