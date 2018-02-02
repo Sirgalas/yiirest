5e4573c71841b86474951c31dc9b6b2d518ccb22
sirgalas <hudos@rambler.ru>
2018-02-02 14:23:53 +0300

    debug redact

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 
 if ($_SERVER['SERVER_NAME'] == "yii2promo.herokuapp.com") {
-    $host = 'ec2-23-23-92-179.compute-1.amazonaws.com';
+    $host = 'ec2-23-23-92-179.compute-1.amazonaws.com port=5432';
     $username = 'jnnbiayopidmsm';
     $password = '35e144d16b2d0348d1c6eff69f3b3f3d320c95a75a9d08a02b278d69cd0fd710';
     $dbname = 'd84g9naft02ago';
