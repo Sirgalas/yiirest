d49f252b8f2ff6d404953c1a37bc9f488f739a9a
sirgalas <hudos@rambler.ru>
2018-02-02 09:53:52 +0300

    redact from heroku

--- config/db.php
+++ config/db.php
@@ -1,10 +1,23 @@
 <?php
 
+$host = $username = $password = $dbname = '';
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+if (isset($url["host"]) && isset($url["user"]) && isset($url["pass"]) && isset($url["path"])) {
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+}else{
+    $host = 'localhost';
+    $username = 'root';
+    $password = '1';
+    $dbname = 'yii2catalog';
+}
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2catalog',
-    'username' => 'root',
-    'password' => '1',
+    'dsn' => 'mysql:host='.$host.';dbname='.$dbname,
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
