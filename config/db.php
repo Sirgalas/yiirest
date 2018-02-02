b508884cfdd46090970ec464917c22cea2797728
sirgalas <hudos@rambler.ru>
2018-02-02 12:58:25 +0300

    debug redact

--- config/db.php
+++ config/db.php
@@ -6,16 +6,18 @@ if ($_SERVER['SERVER_NAME'] == "yii2promo.herokuapp.com") {
     $username = 'jnnbiayopidmsm';
     $password = '35e144d16b2d0348d1c6eff69f3b3f3d320c95a75a9d08a02b278d69cd0fd710';
     $dbname = 'd84g9naft02ago';
+    $nameHost = 'pgsql';
 }else{
     $host = 'localhost';
     $username = 'root';
     $password = '1';
     $dbname = 'yii2catalog';
+    $nameHost = 'mysql';
 }
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host='.$host.';dbname='.$dbname,
+    'dsn' => $nameHost.':host='.$host.';dbname='.$dbname,
     'username' => $username,
     'password' => $password,
     'charset' => 'utf8',
