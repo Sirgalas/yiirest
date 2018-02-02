95f008126c3d973cc40a549d8c26209d86f8e439
sirgalas <hudos@rambler.ru>
2018-02-02 12:52:25 +0300

    debug redact

--- config/db.php
+++ config/db.php
@@ -1,8 +1,7 @@
 <?php
 
-$host = $username = $password = $dbname = '';
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-if (if ($_SERVER['SERVER_NAME'] == "yii2promo.herokuapp.com") {) {
+
+if ($_SERVER['SERVER_NAME'] == "yii2promo.herokuapp.com") {
     $host = 'ec2-23-23-92-179.compute-1.amazonaws.com';
     $username = 'jnnbiayopidmsm';
     $password = '35e144d16b2d0348d1c6eff69f3b3f3d320c95a75a9d08a02b278d69cd0fd710';
